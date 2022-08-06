15e3c95f8323e60b34ed1fce9810dc9ca0eaed1c
GokhanGulen7 <65721303+GokhanGulen7@users.noreply.github.com>
2022-08-06 13:43:12 +0300

    Add files via upload

---
+++ UsluSayilar/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x, y, total = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı: ");
+        x = scanner.nextInt();
+        System.out.print("Üs: ");
+        y = scanner.nextInt();
+
+        for (int i = 1; i <= y; i++) {
+            total *= x;
+        }
+
+        System.out.println("Cevap: " + total);
+    }
+}
